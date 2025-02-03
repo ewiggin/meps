d49823e302e7c61ccd1fb8446bb36bfc8961094c
Mario Martinez <mmartinez@energy-tools.net>
2025-02-03 22:37:28 +0100

    show in movile

--- routes/admin/maps/[id]/[num]/index.tsx
+++ routes/admin/maps/[id]/[num]/index.tsx
@@ -10,9 +10,13 @@ export default function AdminMapsDetailFinalPage(props: any) {
 
   return (
     <>
-      <Breadcrumb title={`Territorio: ${id} - ${num}`} backLink={`/admin/${id}`}></Breadcrumb>
+      <Breadcrumb
+        title={`Territorio: ${id} - ${num}`}
+        backLink={`/admin/${id}`}
+      >
+      </Breadcrumb>
       <div>
-        <div className="w-full min-h-64 hidden md:block">
+        <div className="w-full min-h-64">
           <iframe
             src="https://www.google.com/maps/d/embed?mid=1zONEhOhwP7A1lUEiJqYc3FkB8hzymkE&ll=42.18320406143437%2C2.4822327500000263&z=18"
             frameborder="0"
