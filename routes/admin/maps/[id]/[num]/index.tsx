222ac704657ed8c157091df728d118887b23f8bc
Mario Martinez <mmartinez@energy-tools.net>
2025-02-03 22:22:04 +0100

    fix iframe

--- routes/admin/maps/[id]/[num]/index.tsx
+++ routes/admin/maps/[id]/[num]/index.tsx
@@ -14,7 +14,7 @@ export default function AdminMapsDetailFinalPage(props: any) {
       <div>
         <div className="w-full min-h-64 hidden md:block">
           <iframe
-            src="https://www.google.com/maps/d/viewer?mid=1zONEhOhwP7A1lUEiJqYc3FkB8hzymkE&ll=42.18320406143437%2C2.4822327500000263&z=18"
+            src="https://www.google.com/maps/d/embed?mid=1zONEhOhwP7A1lUEiJqYc3FkB8hzymkE&ll=42.18320406143437%2C2.4822327500000263&z=18"
             frameborder="0"
             width="100%"
             height="500"
