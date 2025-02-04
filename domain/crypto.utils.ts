84d495676b885127dda66399394a46a5e71b41bb
Mario Martinez <mmartinez@energy-tools.net>
2025-02-05 00:18:16 +0100

    add publishers data encrypted ;-)

---
+++ domain/crypto.utils.ts
@@ -0,0 +1,83 @@
+
+export class CryptoUtils {
+
+
+    static async encryptData(data: unknown, defaultIv: Uint8Array) {
+        const encoder = new TextEncoder();
+        // Generar una clave AES-GCM de 256 bits
+        const key = await crypto.subtle.generateKey(
+            { name: "AES-GCM", length: 256 },
+            true,
+            ["encrypt", "decrypt"],
+        );
+
+        // Exportar la clave a formato RAW para reutilizarla
+        const rawKey = await crypto.subtle.exportKey("raw", key);
+        const keyBase64 = btoa(String.fromCharCode(...new Uint8Array(rawKey)));
+
+        console.log("Clave Base64:", keyBase64); // Guarda esta clave para descifrar luego
+        Deno.writeFile("./keyBase64.txt", encoder.encode(keyBase64));
+
+        // Texto a cifrar
+        const jsonData = JSON.stringify(data);
+
+        // Vector de inicialización (IV) - debe ser aleatorio y único por cifrado
+        const iv = defaultIv || crypto.getRandomValues(new Uint8Array(12));
+
+        // Cifrar los datos
+        const encryptedData = await crypto.subtle.encrypt(
+            { name: "AES-GCM", iv },
+            key,
+            encoder.encode(jsonData),
+        );
+
+        // Convertir los datos cifrados a Base64 para almacenarlos en JSON
+        const encryptedBase64 = btoa(
+            String.fromCharCode(...new Uint8Array(encryptedData)),
+        );
+        const ivBase64 = btoa(String.fromCharCode(...iv));
+
+        // Crear un JSON con datos cifrados
+        const encryptedJson = {
+            iv: ivBase64,
+            data: encryptedBase64,
+        };
+
+        console.log("JSON cifrado:", JSON.stringify(encryptedJson, null, 2));
+        return {
+            key: keyBase64,
+            data: encryptedJson
+        }
+    }
+
+    static async decrypt(receivedJson: { iv: string, data: string }, keyBase64: string) {
+
+        const decoder = new TextDecoder();
+
+        // Convertir las claves Base64 a bytes
+        const keyBytes = new Uint8Array([...atob(keyBase64)].map(c => c.charCodeAt(0)));
+        const iv2 = new Uint8Array([...atob(receivedJson.iv)].map(c => c.charCodeAt(0)));
+        const encryptedBytes = new Uint8Array([...atob(receivedJson.data)].map(c => c.charCodeAt(0)));
+
+        // Importar la clave AES-GCM
+        const importedKey = await crypto.subtle.importKey(
+            "raw",
+            keyBytes,
+            { name: "AES-GCM" },
+            false,
+            ["decrypt"]
+        );
+
+        // Descifrar los datos
+        const decryptedData = await crypto.subtle.decrypt(
+            { name: "AES-GCM", iv: iv2 },
+            importedKey,
+            encryptedBytes
+        );
+
+        // Convertir de nuevo a texto JSON
+        const decryptedJson = JSON.parse(decoder.decode(decryptedData));
+        console.log("Datos descifrados:", decryptedJson);
+        return decryptedJson;
+    }
+}
--- routes/admin/maps/[id]/[num]/index.tsx
+++ routes/admin/maps/[id]/[num]/index.tsx
@@ -1,26 +1,29 @@
 import { Breadcrumb } from "../../../../../components/Breadcrumb.tsx";
+import publishers from '../../../../../data/publishers.json' with { type: 'json'};
+import { CryptoUtils } from '../../../../../domain/crypto.utils.ts';
+import { Handlers } from 'https://deno.land/x/fresh@1.7.2/src/server/types.ts';
+
+export const handler: Handlers<unknown> = {
+  async GET(_, ctx) {
+    const { id, num } = ctx.params;
+    return ctx.render({
+      publishers: await CryptoUtils.decrypt(publishers, Deno.env.get('KEY_BASE_64')!),
+      id,
+      num
+    });
+  },
+  async POST(req: Request, ctx) {
+    const { id, num } = ctx.params;
+    const formData = await req.formData();
+    // Redirect when save!
+    return ctx.render({ publishers: await CryptoUtils.decrypt(publishers, Deno.env.get('KEY_BASE_64')!), id, num });
+  },
+};
 
 export default function AdminMapsDetailFinalPage(props: any) {
-  const { id, num } = props.params;
+  const { id, num, publishers } = props.data;
 
-  const data = [
-    {
-      id: "",
-      pub: "Jhon Doe",
-      fromDate: Temporal.Now.plainDateTimeISO().toPlainDate(),
-      toDate: Temporal.Now.plainDateTimeISO().add(
-        Temporal.Duration.from({ days: 31 }),
-      ).toPlainDate(),
-    },
-    {
-      id: "",
-      pub: "Mary Doe",
-      fromDate: Temporal.Now.plainDateTimeISO().subtract({ days: 45 }).toPlainDate(),
-      toDate: Temporal.Now.plainDateTimeISO().subtract({ days: 45 }).add(
-          Temporal.Duration.from({ days: 20 }),
-      ).toPlainDate(),
-    },
-  ];
+  const territory_pubs: any[] = [];
 
   return (
     <>
@@ -34,20 +37,20 @@ export default function AdminMapsDetailFinalPage(props: any) {
         <script src="/map.js"></script>
       </div>
       <div className="container mx-auto max-w-screen-xl px-4 py-12 sm:px-6 sm:py-12 lg:px-8">
-        <div class="overflow-x-auto">
+        <div class="overflow-x-auto pb-8">
           <table class="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
             <thead class="text-left">
             <tr>
-              <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              <th class="whitespace-nowrap px-4 py-2 font-bold text-gray-900">
                 Publicador
               </th>
-              <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              <th class="whitespace-nowrap px-4 py-2 font-bold text-gray-900">
                 Asignado el
               </th>
-              <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              <th class="whitespace-nowrap px-4 py-2 font-bold text-gray-900">
                 Fecha completado
               </th>
-              <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              <th class="whitespace-nowrap px-4 py-2 font-bold text-gray-900">
               </th>
             </tr>
             </thead>
@@ -55,21 +58,24 @@ export default function AdminMapsDetailFinalPage(props: any) {
             <tbody className="divide-y divide-gray-200 bg-gray-200">
               <tr>
                 <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                  <select name="publicador" required={true}>
+                  <select className="px-2 py-2 rounded" name="publisher" required={true}>
                     <option value="null" selected={true}>Selecciona un publicador</option>
-                    <option value="Mario Martínez">Mario Martínez</option>
-                    <option value="Marc Soler">Marc Soler</option>
+                    {
+                      publishers.map(({id, name, lastName}: any) => (
+                          <option value={id}>{name} {lastName}</option>
+                      ))
+                    }
                   </select>
                 </td>
                 <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                  <input type="date" name="fromDate" required={true}/>
+                  <input className="px-2 py-2 rounded"  type="date" name="fromDate" required={true}/>
                 </td>
                 <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                  <input type="date" name="toDate"/>
+                  <input className="px-2 py-2 rounded"  type="date" name="toDate"/>
                 </td>
                 <td className="whitespace-nowrap px-4 py-2 text-gray-700 text-right">
                   <button type="submit">
-                    Guardar
+                    Registrar
                   </button>
                 </td>
               </tr>
@@ -77,7 +83,7 @@ export default function AdminMapsDetailFinalPage(props: any) {
             </form>
 
             <tbody className="divide-y divide-gray-200">
-            {data.map((item) => (
+            {territory_pubs.map((item) => (
                 <tr>
                   <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
                     {item.pub}
